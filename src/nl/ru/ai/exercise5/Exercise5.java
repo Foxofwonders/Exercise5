8abf4a8f79ba9ab045e9aa32cfa5806a1db8e7e2
Unknown <denisevbaalen@gmail.com>
2016-12-16 12:08:38 +0100

    Initial files

---
+++ src/nl/ru/ai/exercise5/Exercise5.java
@@ -0,0 +1,20 @@
+package nl.ru.ai.exercise5;
+
+import static nl.ru.ai.exercise5.Maze.*;
+
+import java.util.ArrayList;
+
+public class Exercise5
+{
+  public static void main(String[] arguments)
+  {
+    ArrayList<Candidate> candidates=new ArrayList<Candidate>();
+    candidates.add(new Candidate(new Attempt(1,7),0));
+    boolean found=false;
+    int c=0;
+    while(c<candidates.size() & !found)
+    {
+      // weave your magic here
+    }
+  }
+}
