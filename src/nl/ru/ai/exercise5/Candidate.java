9fb5c34ff1f5967ec2e995151d878c112da7ab74
Unknown <denisevbaalen@gmail.com>
2016-12-24 16:25:29 +0100

    Added puzzleCandidate

--- src/nl/ru/ai/exercise5/Candidate.java
+++ src/nl/ru/ai/exercise5/Candidate.java
@@ -4,10 +4,17 @@ public class Candidate
 {
   public Attempt attempt;
   public int parentCandidate;
+  public int[][] puzzle;
   
   public Candidate(Attempt attempt, int parentCandidate)
   {
     this.attempt=attempt;
     this.parentCandidate=parentCandidate;
   }
+
+public Candidate(int[][] puzzle, int parentCandidate2) 
+	{
+		this.puzzle=puzzle;
+		this.parentCandidate=parentCandidate2;
+	}
 }
